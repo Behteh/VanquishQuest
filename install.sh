554285c1970d041182bbf566accef3ed08e1f845
Behteh <94149261+Behteh@users.noreply.github.com>
2022-09-29 13:35:53 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,1 @@
-aws s3 cp s3://java-artifacts-vanquishquest/target/project_2-0.0.1-SNAPSHOT.jar /tmp
-sudo mv/tmp/project_2-0.0.1-SNAPSHOT.jar /home/ec2-user/project_2-0.0.1-SNAPSHOT.jar
 nohup java -jar project_2-0.0.1-SNAPSHOT.jar
