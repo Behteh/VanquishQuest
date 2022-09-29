7d2507c8994a1f84ff580cd2a18d630dbdb3ef6f
Behteh <94149261+Behteh@users.noreply.github.com>
2022-09-28 18:51:29 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://java-artifacts-vanquishquest/target/Vanquishquest.war /tmp
-sudo mv/tmp/Vanquishquest.war /usr/share/tomcat/webapps/Vanquishquest.war
+aws s3 cp s3://java-artifacts-vanquishquest/target/project_2-0.1.1-SNAPSHOT.war /tmp
+sudo mv/tmp/Vanquishquest.war /usr/share/tomcat/webapps/project_2-0.1.1-SNAPSHOT.war
 sudo service tomcat restart
